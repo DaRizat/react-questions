b05a10a274bb1487423537790416572bd3e06601
Jack Bishop <jack.darizat@gmail.com>
2019-04-05 17:43:58 -0700

    Fixing output of withNavigation

--- src/hoc/withNavigation.js
+++ src/hoc/withNavigation.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import { NextConsumer, PrevConsumer } from 'context';
 
-const WithNavigation = wrapped => (
-  <NextConsumer>
-    {handleNext => (
-      <PrevConsumer>
-        {(handlePrev) => {
-          const WrappedComponent = React.cloneElement(
-            wrapped,
-            {
-              ...wrapped.props,
-              handleNext,
-              handlePrev,
-            },
-          );
-          return (
-            <WrappedComponent />
-          );
-        }}
-      </PrevConsumer>
-    )}
-  </NextConsumer>
-);
+const WithNavigation = (WrappedComponent) => {
+  const { props } = WrappedComponent;
+  return (
+    <NextConsumer>
+      {handleNext => (
+        <PrevConsumer>
+          {handlePrev => (
+            <WrappedComponent {...props} handleNext={handleNext} handlePrev={handlePrev} />
+          )}
+        </PrevConsumer>
+      )}
+    </NextConsumer>
+  );
+};
 
 export default WithNavigation;
